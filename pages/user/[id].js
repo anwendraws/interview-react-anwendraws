faaa56cb1734cbaa036afb3e0385bd2091a4f6a6
anwendraws <100897759+anwendraws@users.noreply.github.com>
2022-08-14 15:03:41 +0800

    add icon docs
    npm i @chakra-ui/icons

--- pages/user/[id].js
+++ pages/user/[id].js
@@ -1,37 +1,130 @@
 import Image from 'next/image'
 import styles from '../../styles/Home.module.css'
-
 import { useTranslation } from 'react-i18next'
+import { useRouter } from 'next/router'
+import { Container, Button, Stack, Link, Text } from '@chakra-ui/react'
+import { Menu, MenuButton, MenuList, MenuItem } from '@chakra-ui/react'
+import i18next from '../../i18n'
+import { ChevronDownIcon } from '@chakra-ui/icons'
+import Head from 'next/head'
+
+const languageList = [
+  {
+    name: 'English',
+    key: 'en-US',
+  },
+  {
+    name: '中文',
+    key: 'zh-CN',
+  },
+]
+
 
 export default function Home() {
   const { t } = useTranslation('nav')
+  const router = useRouter()
+  const { id } = router.query
+
+  function changeLanguage(key) {
+    i18next.changeLanguage(key)
+  }
   return (
-    <div className={styles.container}>
+
+    // <div className={styles.container}>
+    <Container maxW='-moz-max-content' bg='gray.500' color='white' centerContent>
+      <Head>
+        <title>Make Your Own Blog</title>
+        <meta name="description" content="Make Your Own Blog - Create Your Blog For Free" />
+        <link rel="icon" href="/blog.ico" />
+      </Head>
+      <div className={styles.menuBox}>
+        <Menu >
+          <MenuButton
+            px={4}
+            py={2}
+            transition="all 0.2s"
+            borderRadius="md"
+            borderWidth="1px"
+            background="gray.600"
+            _hover={{ bg: 'gray.700' }}
+            _expanded={{ bg: 'gray.700' }}
+            _focus={{ boxShadow: 'outline' }}
+          >
+            {t('language')}<ChevronDownIcon />
+          </MenuButton>
+          <MenuList background="gray.600">
+            {languageList.map((item) => (
+              <MenuItem
+                key={item.key}
+                onClick={() => changeLanguage(item.key)}
+                background="gray.600"
+                _hover={{ bg: 'gray.700' }}
+                _expanded={{ bg: 'gray.800' }}
+                _focus={{ boxShadow: 'outline' }}>
+                {item.name}
+              </MenuItem>
+            ))}
+          </MenuList>
+        </Menu>
+        <Menu >
+          <MenuButton
+            px={4}
+            py={2}
+            mt={5}
+            transition="all 0.2s"
+            borderRadius="md"
+            borderWidth="1px"
+            background="gray.600"
+            _focus={{ boxShadow: 'outline' }}
+
+          >
+            {t('home')}
+          </MenuButton>
+        </Menu>
+        <Menu >
+          <MenuButton
+            px={4}
+            py={2}
+            transition="all 0.2s"
+            borderRadius="md"
+            borderWidth="1px"
+            background="gray.600"
+            _focus={{ boxShadow: 'outline' }}
+
+          >
+            {t('faq')}
+          </MenuButton>
+        </Menu>
+      </div>
       <main className={styles.main}>
-        <h1 className={styles.title}>Welcome to {t('home')}, USER</h1>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Status 1 &rarr;</h2>
-            <p>Page height:</p>
-            <p>Page width:</p>
-          </a>
-
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Status 2 &rarr;</h2>
-            <p>Mouse position:</p>
-          </a>
-
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Status 3 &rarr;</h2>
-            <p>Current date:</p>
-          </a>
-
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Status 4 &rarr;</h2>
-            <p>Your IP:</p>
-          </a>
-        </div>
+        <Text fontSize='6xl'>{t('Welcome to Home')}, {id}</Text>
+        <Stack spacing={4} direction='row' align='center' mt='70px'>
+          <Button colorScheme='blackAlpha' size='lg' height='200px' width='250px' border='2px' borderColor='whiteAlpha.800'>
+            <Link href="https://nextjs.org/docs">
+              <Text fontSize='3xl'>{t('Status 1')}  &rarr;</Text>
+              <Text fontSize='2xl'>{t('Page height:')}</Text>
+              <Text fontSize='2xl'>{t('Page width:')}</Text>
+            </Link>
+          </Button>
+          <Button colorScheme='blackAlpha' size='lg' height='200px' width='250px' border='2px' borderColor='whiteAlpha.800'>
+            <Link href="https://nextjs.org/docs">
+              <Text fontSize='3xl'>{t('Status 2')} &rarr;</Text>
+              <Text fontSize='2xl'>{t('Mouse position:')}</Text>
+            </Link>
+          </Button>
+          <Button colorScheme='blackAlpha' size='lg' height='200px' width='250px' border='2px' borderColor='whiteAlpha.800'>
+            <Link href="https://nextjs.org/docs">
+              <Text fontSize='3xl'>{t('Status 3')} &rarr;</Text>
+              <Text fontSize='2xl'>{t('Current date:')}</Text>
+            </Link>
+          </Button>
+          <Button colorScheme='blackAlpha' size='lg' height='200px' width='250px' border='2px' borderColor='whiteAlpha.800'>
+            <Link href="https://nextjs.org/docs">
+              <Text fontSize='3xl'>{t('Status 4')} &rarr;</Text>
+              <Text fontSize='2xl'>{t('Your IP:')}</Text>
+            </Link>
+          </Button>
+        </Stack>
       </main>
 
       <footer className={styles.footer}>
@@ -40,13 +133,14 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          {t('Powered by')}{' '}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
-    </div>
+    </Container>
+
   )
 }
 
