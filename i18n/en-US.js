3ff9f9d513379811d3dcbb72e90ff86179717dc7
anwendraws <100897759+anwendraws@users.noreply.github.com>
2022-08-14 15:08:34 +0800

    Add language conversion code

--- i18n/en-US.js
+++ i18n/en-US.js
@@ -1,6 +1,18 @@
 export const all = {
   nav: {
-    home: 'Home',
-    faq: 'FAQ',
+    'home': 'Home',
+    'faq': 'FAQ',
+    'language': "Language",
+    'Status 1': "Status 1",
+    'Welcome to Home': "Welcome to Home",
+    'Page height:': "Page height:",
+    'Page width:': "Page width:",
+    'Status 2': "Status 2",
+    'Status 3': "Status 3",
+    'Status 4': "Status 4",
+    'Mouse position:': "Mouse position:",
+    'Current date:': "Current date:",
+    'Your IP:': "Your IP:",
+    'Powered by': "Powered by",
   },
 }
--- i18n/zh-CN.js
+++ i18n/zh-CN.js
@@ -1,6 +1,18 @@
 export const all = {
   nav: {
-    home: '主页',
-    faq: '常见问题',
+    'home': '主页',
+    'faq': '常见问题',
+    'language': "切换语言",
+    'Status 1': "状态1",
+    'Welcome to Home': "欢迎来到主页",
+    'Page height:': "页面高度：",
+    'Page width:': "页面宽度：",
+    'Status 2': "状态2",
+    'Status 3': "状态3",
+    'Status 4': "状态4",
+    'Mouse position:': "鼠标位置：",
+    'Current date:': "当前日期：",
+    'Your IP:': "你的IP：",
+    'Powered by': "技术支持提供方",
   },
 }
